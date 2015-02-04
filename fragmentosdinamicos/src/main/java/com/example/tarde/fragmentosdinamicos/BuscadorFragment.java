a8b378f7c19531ad396c295cebe296e435fee56f
unknown <tarde@mail.com>
2015-02-04 18:27:13 +0100

    fragmentos dinamicos y dialogos

---
+++ fragmentosdinamicos/src/main/java/com/example/tarde/fragmentosdinamicos/BuscadorFragment.java
@@ -0,0 +1,28 @@
+package com.example.tarde.fragmentosdinamicos;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BuscadorFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
